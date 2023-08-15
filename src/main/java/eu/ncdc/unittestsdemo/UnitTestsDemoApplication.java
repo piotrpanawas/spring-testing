d4c395e05a0ee70fd86baa7f16d413cc6a4d6a2a
Piotr Panawas <piotr.panawas@ncdc.pl>
2023-08-15 12:29:33 +0200

    Init

---
+++ src/main/java/eu/ncdc/unittestsdemo/UnitTestsDemoApplication.java
@@ -0,0 +1,13 @@
+package eu.ncdc.unittestsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/ncdc/unittestsdemo/UnitTestsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ncdc.unittestsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
